c4ddb0e29bd38613141ccfff52f56a0a3fa9c703
Falanteris <rayhanrts@gmail.com>
2019-12-11 19:38:59 +0700

    Modified registerEventListener.js for actual event listening.

--- registerEventListener.js
+++ registerEventListener.js
@@ -14,33 +14,42 @@ fs.writeFile("/var/cvss.json",JSON.stringify(status),(err)=>{
 	}
 	console.log("Instantiate Process Finished")
 });
-fs.watchFile("/var/cvss.json",(old,curr)=>{
+function add_watcher(to_watch,kill_command,start_command){
+fs.watchFile(to_watch,(old,curr)=>{
 
-	fs.readFile("/var/cvss.json",(err,data)=>{
+	fs.readFile(to_watch,(err,data)=>{
 		if(err) {console.log(err);return;}
 		try{
 			let state = JSON.parse(data.toString());
 			if(!state.active && !state.hasOwnProperty("termination")){
 				let termination = new Date();
 				state.termination = termination.toString();
-				spawn("forever",["stop","updater.js"])
-				fs.writeFile("/var/cvss.json",JSON.stringify(state),()=>{
+				kill_command.forEach((command)=>{
+					spawn(command[0],command[1])
+				})
+				fs.writeFile(to_watch,JSON.stringify(state),()=>{
 					console.log("Done writing changes")
 				})
+				
 				console.log(state)
 				return;
 			}
 			if(state.active && state.hasOwnProperty("termination")){
 				state.lastTermination = state.termination
 				delete state.termination;
-				sp = spawn("forever",["start","updater.js"])
+				//sp = spawn("forever",["start","updater.js"])
+				start_command.forEach((command)=>{
+				let sp =spawn(command[0],command[1]);
+				
 				sp.stderr.on("data",(d)=>{
 					console.log(d.toString())
 				})
 				sp.stdout.on('data',(d)=>{
 					console.log(d.toString())
 				})
-				fs.writeFile("/var/cvss.json",JSON.stringify(state),()=>{
+
+				})
+				fs.writeFile(to_watch,JSON.stringify(state),()=>{
                                         console.log("Done writing changes")
                                 })
 				console.log(state)
@@ -51,6 +60,8 @@ fs.watchFile("/var/cvss.json",(old,curr)=>{
 		}
 	})
 })
+}
+add_watcher("/var/cvss.json",[["forever",["stop","updater.js"]]],[["forever",["start","updater.js"]]])
 
 //setInterval(()=>{
 //	console.log("Running Diagnostic")
