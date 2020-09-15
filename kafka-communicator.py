f2f8742f9b0855b3d792bef0ff52169fcdf53aa7
Falanteris <rayhanrts@gmail.com>
2020-09-15 11:22:45 +0700

    Added IP and URL data to ES as well

--- kafka-communicator.py
+++ kafka-communicator.py
@@ -83,6 +83,8 @@ def process_message(msg):
 		new_instance["vul_tag"] = msg["vul"]
 		new_instance["SUMMARIZE_RESULT"] = call_summarizer(arg)
 		new_instance["payload"] = msg["payload"]
+		new_instance["source"] = msg["ip"]
+		new_instance["path"] = msg["url"]
 	return json.dumps(new_instance)
 def send_cached_data():
 	with open("kc.log","r+") as f:
