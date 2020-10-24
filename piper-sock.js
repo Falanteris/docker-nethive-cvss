37e1ae3eb19adf9322c44dcbbf184502d985e41d
Falanteris <rayhanrts@gmail.com>
2020-10-24 15:37:20 +0700

    Added More Vul Types

--- piper-sock.js
+++ piper-sock.js
@@ -1,128 +1,170 @@
-let net = require("net");
-let {avarice} = require("./piper2.0");
-let {detectPR} = require("./piper2.0");
-let {getSource} = require("./piper2.0");
-let {calculator} = require("./piper2.0");
-let sock = ""
-let unlink = require('fs').unlinkSync
-let link = require('fs').linkSync
-let isAccessible = require('fs').accessSync;
-let readline = require("readline")
-let rl = readline.createInterface({
-	input:process.stdin,
-	output:process.stdout
-})
-function socketExists(){
-	let exist;
-	try{
-		isAccessible(sock)
-		exist = true;
-	}catch(err){
-		
-	}
-	return exist;
+let calc = require("./learn");
+var spawn = require("child_process");
+
+
+function calculator(string){
+	let calc = spawn.spawnSync("python3",["cvss-calc/scripter.py",string])
+	//console.log(calc.stderr.toString() || "No error")
+	res = calc.stdout == undefined?0.0:calc.stdout;
+	return res.toString();
 }
-rl.on("SIGINT",()=>{
-	console.log("Stopping")
+
+async function https_get(redir){
 	
-	if(socketExists()){
-		unlink(sock);
-	}
-	process.exit()
-})
+		return new Promise((resolve,reject)=>{
 
-function validator(data){
-	let regex = /ip=([0-9]{1,3}\.){3}[0-9]{1,3}&url=\S+&vul=[A-Z]+/g
-	return regex.test(data);
-}
-function deserializer(data){
-	data = data.split("&")
-	let unser = {}
-	for(i in data){
-		let item = data[i].split("=")
-		unser[item[0]] = item[1];
-	}
-	return unser;
-}
-function createServerHandler(handler,sockName,maxCons=undefined){
-console.log("Creating handler ..");
-sock = sockName
-let server = net.createServer((c)=>{
-	//handlers
-	if(!isNaN(maxCons) && maxCons>0){
-		server.getConnections((err,count)=>{
-			if(count > maxCons){
-				c.destroy();
-				return;
-			}
-			c.on("data",(data)=>{
-				handler(data);
-			})
-			c.on("close",()=>{
-				console.log("Connection terminated");
-			})
-			c.on("error",()=>{
-				console.log("[+] Socket is missing.. attempting to recover from abrupt termination..")
-				if(!socketExists()){
-			
-				link(sock,sock);
-			}
-			console.log("[+] Recovery success ..")
-			})
+		https.request("https://"+redir,(data)=>{
+						let av = {}
+
+						let {statusCode} = data
+							if (statusCode !== 200) {
+								console.log("test")
+               					 error = new Error('Request Failed.\n' +
+                      				`Status Code: ${statusCode}`);
+                					av.result = "L"
+            				    av.statusCode = statusCode
+								reject(error)
+
+ 		            }
+				else{
+					console.log("success")
+                	av.result = "N"
+                	av.statusCode = statusCode
+                	resolve(av)
+
+				}
+
+					})
 		})
-		return;
+}
+
+async function http_get(redir){
+	
+		return new Promise((resolve,reject)=>{
+
+		http.request("http://"+redir,(data)=>{
+						let av = {}
+
+						let {statusCode} = data
+							if (statusCode !== 200) {
+								console.log("test")
+               					 error = new Error('Request Failed.\n' +
+                      				`Status Code: ${statusCode}`);
+                					av.result = "L"
+            				    av.statusCode = statusCode
+								reject(error)
+
+ 		            }
+				else{
+					console.log("success")
+                	av.result = "N"
+                	av.statusCode = statusCode
+                	resolve(av)
+
+				}
+
+					})
+		})
+}
+function callback(res){
+const { statusCode } = res;
+  const contentType = res.headers['content-type'];
+
+  let error;
+  if (![200,301].includes(statusCode) ) {
+    error = new Error('Request Failed.\n' +
+                      `Status Code: ${statusCode}`);
+  } 
+  if (error) {
+    console.error(error.message);
+    // Consume response data to free up memory
+    res.resume();
+    return;
+  }
+
+  res.setEncoding('utf8');
+  let rawData = '';
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    try {
+    	console.log(res)
+      console.log(rawData)
+      
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
+}
+function detectPR(urlsplit,error_reports,cb){
+	let {spawnSync} = require("child_process")
+	let pr = spawnSync("python3",["requester.py","http://"+urlsplit])
+	if(pr.stdout.toString()!="200"){
+		return {result:'L'}
 	}
-	c.on("data",(data)=>{
-		handler(data);
-	})
-	c.on("close",()=>{
-		console.log("Connection terminated");
-	})
-	c.on("error",()=>{
-		console.log("[+] Socket is missing.. attempting to recover from abrupt termination..")
-		if(!socketExists()){
-			
-			link(sock,sock);
+
+	return {result:'N'}
+	
+}
+function getSource(src){
+	//console.log(src)
+	try{
+		
+		let sp = calc(src);
+		let item = JSON.parse(sp)
+		
+		
+		return item;
+	}catch(exception){
+		return exception;
+	}
+}
+
+function avarice(ip,error_reports){
+	let av;
+	let {promisify} = require("util")
+	let {lookup} = require("dns")
+	let dns_lookup = promisify(lookup)
+	return function (callbacker){
+
+	dns_lookup(ip).then((result)=>{
+
+	try{
+	
+	av = spawn.spawnSync("AVdetector/avarice",[result.address]);
+	}catch(err){
+		error_reports = stackTrace
+		return {
+
+			status:undefined,
+			stackTrace:error_reports
 		}
-		console.log("[+] Recovery success ..")
-	})
-})
-/*
-process.on("exit",()=>{
-
-	if(socketExists()){
-		unlink(sock);
 	}
-})*/
+	let util = require("util")
 
-server.on("error",(err)=>{
 
-	console.log(err);
-	if(socketExists()){
-		unlink(sock)
+	if(av.stdout != ''){
+		av = av.stdout.toString()[0];
 	}
-})
-return server;
-}
-process.on("SIGTERM",()=>{
-	server.close(()=>{
-		process.exit()	
-	})
-})
-process.on("SIGINT",()=>{
-	server.close(()=>{
-		process.exit();
-})
-})
-if(socketExists()){
-		//so that it can work properly with forever daemon
-		//since it doesn't send proper kill signal for the process events to be emitted
-             unlink(sock)
-}
-module.exports = createServerHandler;
-/*
-server.listen(sock,()=>{
-	console.log("[+] Server listening");
-});
-*/
 
+	else{
+
+				error_reports = av.stderr.toString()
+				callbacker({
+					status:undefined,
+					stackTrace:error_reports}
+				);
+	}
+	
+
+	
+
+	callbacker({status:1,result:av});
+	})
+	}
+}
+module.exports = {
+	avarice:avarice,
+	detectPR:detectPR,
+	calculator:calculator,
+	getSource:getSource
+}
